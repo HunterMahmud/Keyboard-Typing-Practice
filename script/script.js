36c5c22e99f4c1a4ffa5a732661bede62f44e367
huntermahmud <mahmudthedatahunter@gmail.com>
2024-02-15 17:08:22 +0600

    All issue solvec

--- script/script.js
+++ script/script.js
@@ -1,15 +1,19 @@
-document.getElementById("home").addEventListener('keyup',enter);
+// Developed by Hasan Al Mahmud
 
+addEventListener('keyup',enter);
 function enter(e){
-    if(e.key === "Enter"){
+    let hidden = [...document.getElementById('home').classList];
+    if(e.key === "Enter" && !hidden.includes('hidden')){
         play();
     }
 }
+
 
 function play() {
     document.getElementById('current-score').innerText = 0;
     document.getElementById("life").innerText = 5;
   addClass("home", "hidden");
+  addClass("score", "hidden");
   removeClass("playground", "hidden");
   game();
 }
@@ -18,15 +22,15 @@ function game() {
   let alphabet = getRandomLetter();
   addClass(alphabet, "bg-red-500");
   document.getElementById("target-letter").innerText = alphabet;
-  addEventListener('keyup',typing)
+  addEventListener('keyup',typing);
 }
 
 function typing(e){
     let alphabet = document.getElementById("target-letter").innerText.toLowerCase();
-    if(e.key.toLowerCase() === 'Escape'){
+    if(e.key.toLowerCase() === 'escape'){
         showScore(alphabet);
     }
-    if(e.key===alphabet){
+    else if(e.key===alphabet){
         removeClass(alphabet, "bg-red-500");
         setScore('current-score');
         game();
